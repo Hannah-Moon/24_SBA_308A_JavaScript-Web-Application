4a8bd762ccf3718cb942b22d16198ada38f9c224
Hannah-Moon <59512737+Hannah-Moon@users.noreply.github.com>
2024-04-09 16:38:32 -0400

    fix

--- googleMap.js
+++ googleMap.js
@@ -1,4 +1,3 @@
-
 // Initialize and add the map
 let map;
 
@@ -12,7 +11,7 @@ export async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at Uluru 
   map = new Map(document.getElementById("map"), {
     zoom: 13,
     center: new google.maps.LatLng(40.4412027129961, -79.91779536531955),
@@ -22,7 +21,8 @@ export async function initMap() {
   });
 
   // Bouncing logo
-  let image = '/assets/doogieLogo.png';
+  
+  let image = 'assets/doogieLogo.png';
   let beachMarker = new google.maps.Marker({
     position: {lat: 40.4412027129961, lng: -79.91779536531955},
     map: map,
@@ -30,17 +30,82 @@ export async function initMap() {
     icon: image
   });
 
-  // The marker, positioned at Uluru
-  let marker = new AdvancedMarkerView({
+  // The marker, positioned at Pittsburgh
+  let marker = new AdvancedMarkerView({ // --> Keep causing issues. 
+  // let marker = new google.maps.Marker({
+
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Pittsburgh",
   });
 }
-
-
 
 initMap();
 
 
 
+// --------------- { TRY WITH NEW WITH MANY MARKS}
+
+// The following example creates a marker in Stockholm, Sweden using a DROP
+// animation. Clicking on the marker will toggle the animation between a BOUNCE
+// animation and no animation.
+// let marker;
+
+// let map;
+
+// function initMap() {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 13,
+//     center: { lat: 59.325, lng: 18.07 },
+//   });
+
+//   marker = new google.maps.Marker({
+//     map,
+//     draggable: true,
+//     animation: google.maps.Animation.DROP,
+//     position: { lat: 59.327, lng: 18.067 },
+//   });
+//   marker.addListener("click", toggleBounce);
+// }
+
+// function toggleBounce() {
+//   if (marker.getAnimation() !== null) {
+//     marker.setAnimation(null);
+//   } else {
+//     marker.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
+
+// window.initMap = initMap;
+
+// --------------- { TRY WITH Dale Seo's way}
+
+// window.initMap = function () {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 37.5400456, lng: 126.9921017 },
+//     zoom: 10,
+//   });
+
+//   const malls = [
+//     { label: "C", name: "코엑스몰", lat: 37.5115557, lng: 127.0595261 },
+//     { label: "G", name: "고투몰", lat: 37.5062379, lng: 127.0050378 },
+//     { label: "D", name: "동대문시장", lat: 37.566596, lng: 127.007702 },
+//     { label: "I", name: "IFC몰", lat: 37.5251644, lng: 126.9255491 },
+//     { label: "L", name: "롯데월드타워몰", lat: 37.5125585, lng: 127.1025353 },
+//     { label: "M", name: "명동지하상가", lat: 37.563692, lng: 126.9822107 },
+//     { label: "T", name: "타임스퀘어", lat: 37.5173108, lng: 126.9033793 },
+//   ];
+
+//   const bounds = new google.maps.LatLngBounds();
+
+//   malls.forEach(({ label, name, lat, lng }) => {
+//     const marker = new google.maps.Marker({
+//       position: { lat, lng },
+//       label,
+//       map,
+//     });
+//     bounds.extend(marker.position);
+//   });
+
+//   map.fitBounds(bounds);
+// };
