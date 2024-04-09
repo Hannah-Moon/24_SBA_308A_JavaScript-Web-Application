3e83589d358360a46e3b506419cf190278cac104
Hannah-Moon <59512737+Hannah-Moon@users.noreply.github.com>
2024-04-09 13:41:47 -0400

    fixed dog api

--- index.js
+++ index.js
@@ -29,9 +29,11 @@ seeMoreBtn.addEventListener('click', function() {
     callDoggie();
 });
 
+
+
 async function callDoggie() {
     try {
-        const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_aI5LrRMrx5IyHNmecCqRIZWpO0dkM6Tle5485rhNiKHy7trxF7T3cvMMyjnELPfB");
+        const response = await fetch("https://api.thedogapi.com/v1/images/search?limit=20&api_key=live_aI5LrRMrx5IyHNmecCqRIZWpO0dkM6Tle5485rhNiKHy7trxF7T3cvMMyjnELPfB");
         const data = await response.json();
         
         let imgUrl1 = data[0].url;
